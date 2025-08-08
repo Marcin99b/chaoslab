eecad5742afbbd8df9611c6e3a6c9115e631722d
Marcin99b <marcin.bigos99@gmail.com>
2025-08-08 17:21:44 +0200

    port in cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    port: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("port: {}", args.port);
 }
