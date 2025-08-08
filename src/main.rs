7ba643bd85d1afa185538ec20d16ff192e7311c9
Marcin99b <marcin.bigos99@gmail.com>
2025-08-08 23:25:46 +0200

    read http requests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
 use clap::Parser;
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+    str,
+};
 
 #[derive(Parser)]
 struct Cli {
     port: i32,
 }
 
-fn main() {
-    let args = Cli::parse();
+fn main() -> std::io::Result<()> {
+    let port = Cli::try_parse().map_or(1234, |x| x.port);
+    println!("port: {}", port);
+    let address = format!("127.0.0.1:{}", port);
+    println!("address: {}", address);
+    let listener = TcpListener::bind(address)?;
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
 
-    println!("port: {}", args.port);
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    for item in http_request {
+        println!("{}", item);
+    }
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
