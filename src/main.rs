9e329b6171a1594c39877ade3b2ae347017d3c3e
Marcin99b <marcin.bigos99@gmail.com>
2025-08-09 15:15:37 +0200

    working proxy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 use std::{
-    io::{BufRead, BufReader, Read, Write},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
-    str,
+    sync::Arc,
+    thread,
 };
 
 #[derive(Parser)]
@@ -23,26 +24,63 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let http_request = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<_>>()
-        .join("\r\n");
-    let http_request = format!("{}\r\n\r\n", http_request);
+fn handle_client(stream: TcpStream) {
+    use std::sync::Mutex;
 
-    println!("{}", http_request);
+    let target_stream = Arc::new(Mutex::new(TcpStream::connect("127.0.0.1:7176").unwrap()));
+    let client_stream = Arc::new(Mutex::new(stream));
 
-    let mut send_stream = TcpStream::connect("127.0.0.1:7176").unwrap();
-    send_stream.write_all(http_request.as_bytes()).unwrap();
+    // Proxy client -> target
+    let t1 = {
+        let client = Arc::clone(&client_stream);
+        let target = Arc::clone(&target_stream);
+        thread::spawn(move || {
+            let mut buf = [0u8; 4096];
+            loop {
+                let mut client = client.lock().unwrap();
+                match client.read(&mut buf) {
+                    Ok(0) => {
+                        let target = target.lock().unwrap();
+                        let _ = target.shutdown(std::net::Shutdown::Write);
+                        break;
+                    }
+                    Ok(n) => {
+                        let mut target = target.lock().unwrap();
+                        if target.write_all(&buf[..n]).is_err() {
+                            break;
+                        }
+                    }
+                    Err(_) => break,
+                }
+            }
+        })
+    };
 
-    let mut response = String::new();
-    let mut buf_reader_send = BufReader::new(&send_stream);
-    buf_reader_send.read_to_string(&mut response).unwrap();
+    let t2 = {
+        let client = Arc::clone(&client_stream);
+        let target = Arc::clone(&target_stream);
+        thread::spawn(move || {
+            let mut buf = [0u8; 4096];
+            loop {
+                let mut target = target.lock().unwrap();
+                match target.read(&mut buf) {
+                    Ok(0) => {
+                        let client = client.lock().unwrap();
+                        let _ = client.shutdown(std::net::Shutdown::Write);
+                        break;
+                    }
+                    Ok(n) => {
+                        let mut client = client.lock().unwrap();
+                        if client.write_all(&buf[..n]).is_err() {
+                            break;
+                        }
+                    }
+                    Err(_) => break,
+                }
+            }
+        })
+    };
 
-    println!("{}", response);
-
-    stream.write_all(response.as_bytes()).unwrap();
+    let _ = t1.join();
+    let _ = t2.join();
 }
