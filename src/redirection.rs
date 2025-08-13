a8d4d1bda9fd4b20bd67eb99f5fe5566b190af66
Marcin99b <marcin.bigos99@gmail.com>
2025-08-13 14:38:59 +0200

    use mutex to change redirection mode

--- src/redirection.rs
+++ src/redirection.rs
@@ -1,12 +1,13 @@
 use core::time;
 use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 
 pub struct Redirection {
     pub listening_port: i32,
     pub target_address: String,
-    pub mode: RedirectionMode,
+    pub mode: Arc<Mutex<RedirectionMode>>,
 }
 
 #[derive(Clone)]
@@ -21,51 +22,59 @@ impl Redirection {
         Redirection {
             listening_port,
             target_address,
-            mode: RedirectionMode::Off,
+            mode: Arc::new(Mutex::new(RedirectionMode::Off)),
         }
     }
     pub fn init(&self) -> io::Result<JoinHandle<()>> {
         let listening_port = self.listening_port;
         let target_address = self.target_address.clone();
-        let mode = self.mode.clone();
+        let mode = Arc::clone(&self.mode);
         let t = thread::spawn(move || {
             let address = format!("127.0.0.1:{}", listening_port);
             let listener = TcpListener::bind(address).unwrap();
             for stream in listener.incoming() {
                 if let Ok(s) = stream {
-                    match mode.clone() {
+                    let res = match mode.lock().unwrap().clone() {
                         RedirectionMode::Started => {
                             Redirection::handle_client(s, target_address.clone())
                         }
                         RedirectionMode::Slowed(x) => {
                             thread::sleep(time::Duration::from_millis(x));
-                            Redirection::handle_client(s, target_address.clone());
+                            Redirection::handle_client(s, target_address.clone())
                         }
-                        RedirectionMode::Off => s.shutdown(std::net::Shutdown::Write).unwrap(),
+                        RedirectionMode::Off => {
+                            let _ = s.shutdown(std::net::Shutdown::Write);
+                            Ok(())
+                        }
                     };
+                    if let Err(e) = res {
+                        eprintln!("Error handling client: {}", e);
+                    }
                 }
             }
         });
         Ok(t)
     }
-    pub fn start(&mut self) {
-        let _ = std::mem::replace(&mut self.mode, RedirectionMode::Started);
+    pub fn start(&self) {
+        let mut mode = self.mode.lock().unwrap();
+        *mode = RedirectionMode::Started;
     }
 
-    pub fn stop(&mut self) {
-        let _ = std::mem::replace(&mut self.mode, RedirectionMode::Off);
+    pub fn stop(&self) {
+        let mut mode = self.mode.lock().unwrap();
+        *mode = RedirectionMode::Off;
     }
 
-    pub fn slow(&mut self, ms: u64) {
-        let _ = std::mem::replace(&mut self.mode, RedirectionMode::Slowed(ms));
+    pub fn slow(&self, ms: u64) {
+        let mut mode = self.mode.lock().unwrap();
+        *mode = RedirectionMode::Slowed(ms);
     }
+    fn handle_client(stream: TcpStream, target_address: String) -> io::Result<()> {
+        let target_stream = TcpStream::connect(target_address)?;
 
-    fn handle_client(stream: TcpStream, target_address: String) {
-        let target_stream = TcpStream::connect(target_address).unwrap();
-
-        let mut client_read = stream.try_clone().unwrap();
+        let mut client_read = stream.try_clone()?;
         let mut client_write = stream;
-        let mut target_read = target_stream.try_clone().unwrap();
+        let mut target_read = target_stream.try_clone()?;
         let mut target_write = target_stream;
 
         // Thread: client -> target
@@ -108,5 +117,6 @@ impl Redirection {
 
         let _ = t1.join();
         let _ = t2.join();
+        Ok(())
     }
 }
