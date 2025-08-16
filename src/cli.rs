2ab1892a01027a581e3989fc09e39e9f572c0009
Marcin99b <marcin.bigos99@gmail.com>
2025-08-17 01:01:19 +0200

    println - show params

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,7 @@
 use std::{io::Write, net::TcpStream};
 
 pub fn send(message: String, address: String) {
+    println!("sending request {}", message);
     let mut stream = TcpStream::connect(address).unwrap();
     let buf = message.as_bytes();
     stream.write_all(&buf).unwrap();
--- src/engine.rs
+++ src/engine.rs
@@ -26,6 +26,7 @@ impl Engine {
         let threads = Arc::clone(&self.threads);
         let redirections = Arc::clone(&self.redirections);
         thread::spawn(move || {
+            println!("starting engine {}", address);
             let listener = TcpListener::bind(address).unwrap();
             for stream in listener.incoming() {
                 let mut buf = [0u8; 4096];
@@ -37,7 +38,7 @@ impl Engine {
                         }
                         Ok(n) => {
                             let request = str::from_utf8(&buf[..n]).unwrap();
-                            println!("{}", request);
+                            println!("Incoming {}", request);
                             let params = parse_args_from_string(request.to_string());
 
                             for param in params {
