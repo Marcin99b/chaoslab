eabaa513d9aff7e9f90d1287f724d0f42ae871cc
Marcin99b <marcin.bigos99@gmail.com>
2025-08-16 21:38:52 +0200

    process params

--- src/engine.rs
+++ src/engine.rs
@@ -17,7 +17,7 @@ impl Engine {
         }
     }
 
-    pub fn start(&self, address: String) {
+    pub fn start(&mut self, address: String) {
         let listener = TcpListener::bind(address).unwrap();
         for stream in listener.incoming() {
             let mut buf = [0u8; 4096];
@@ -31,6 +31,20 @@ impl Engine {
                         let request = str::from_utf8(&buf[..n]).unwrap();
                         println!("{}", request);
                         let params = parse_args_from_string(request.to_string());
+
+                        for param in params {
+                            println!(
+                                "Start redirection {} | 127.0.0.1:{} -> {}",
+                                param.name, param.expose, param.target
+                            );
+                            let r = Redirection::new(param.expose, param.target.clone());
+                            let t = r.init().unwrap();
+                            self.threads.push(t);
+
+                            r.start();
+                            r.slow(200);
+                            self.redirections.push(r);
+                        }
                     }
                     Err(_) => break,
                 }
